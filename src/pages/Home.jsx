7ecd5db1befde4b254579e6d302c58d679daaec3
Andy Schunke <schunke.andy@gmail.com>
2023-03-04 09:33:44 +0100

    searchbar bugfix and hover update on each card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import Student from '@/components/Student';
-import searchStudent from '@/lib/studentInformation';
-import React, { useEffect, useRef, useState } from 'react';
-import { CgSearch } from 'react-icons/cg';
+import Student from "@/components/Student";
+import searchStudent from "@/lib/studentInformation";
+import React, { useEffect, useRef, useState } from "react";
+import { CgSearch } from "react-icons/cg";
 
 const Home = () => {
   const [active, setactive] = useState(false);
-  const [query, setquery] = useState('');
+  const [query, setquery] = useState("");
   let filtered = searchStudent(query);
-  const searchElem = useRef('');
-  const headerElem = useRef('');
+  const searchElem = useRef("");
+  const headerElem = useRef("");
 
   const handleSearch = (input) => {
-    const query = input.trim().replaceAll('-', '');
+    const query = input.trim().replaceAll("-", "");
     setquery(query);
   };
 
@@ -23,27 +23,27 @@ const Home = () => {
       const headerRect = header?.getBoundingClientRect();
 
       if (rect.top < 50 && !active) setactive(true);
-      else if (headerRect.bottom > 180 && query === '') setactive(false);
+      else if (headerRect.bottom > 180) setactive(false);
     }
   };
 
   useEffect(() => {
     if (window) {
-      window.addEventListener('scroll', handleScroll);
+      window.addEventListener("scroll", handleScroll);
     }
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [active, query]);
 
   return (
     <>
       <header ref={headerElem} onResize={(e) => handleScroll(e)}>
-        <div className='header-content'>
-          <div className='content-wrapper'>
+        <div className="header-content">
+          <div className="content-wrapper">
             <h1>
               Find possible candidates from Masterschool for
-              <span className='black'> your Company</span>
+              <span className="black"> your Company</span>
             </h1>
             <p>
               Masterschool is a global network of success-based schools led by
@@ -54,24 +54,24 @@ const Home = () => {
               finding a job.
             </p>
           </div>
-          <img className='hero-avatar' src='/AgamsPicture.png' alt='' />
+          <img className="hero-avatar" src="/AgamsPicture.png" alt="" />
         </div>
         <div
           ref={searchElem}
           onResize={(e) => handleScroll(e)}
-          className={!active ? 'search-wrapper' : 'search-wrapper active'}>
-          <CgSearch className='search-icon' />
+          className={!active ? "search-wrapper" : "search-wrapper active"}>
+          <CgSearch className="search-icon" />
           <input
-            type='search'
+            type="search"
             value={query}
             onChange={(e) => handleSearch(e.target.value)}
-            placeholder='Country, Role, Local, Remote'
+            placeholder="Country, Role, Local, Remote"
           />
         </div>
       </header>
 
       <main>
-        <ul className='students-wrapper'>
+        <ul className="students-wrapper">
           {filtered &&
             filtered.map((student, index) => (
               <li key={index}>
