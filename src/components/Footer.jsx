92e9600ca2ff90d8015345c43c913d9ff315aa76
Andy Schunke <schunke.andy@gmail.com>
2023-03-06 11:36:01 +0100

    footer link in new windows

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
       <p>
         Made with Next.js{" "}
         <span>
-          <a href="https://github.com/D-Nayte/search-students-app">@Github</a>
+          <a
+            href="https://github.com/D-Nayte/search-students-app"
+            target="_blank">
+            @Github
+          </a>
         </span>
       </p>
     </footer>
